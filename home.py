fc76a7523f439848e8c6cd907ce283a62d622e44
CodeWithAnjalii <ay4567430@gmail.com>
2025-06-08 12:14:32 +0530

    1DArray

---
+++ home.py
@@ -0,0 +1,6 @@
+import numpy as np
+# This code snippet is intended to demonstrate the correct way to create a NumPy array.
+
+arr = np.array([1,3,2,4,2,7])
+# The array is created with the specified elements.
+print(arr)
